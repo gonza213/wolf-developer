3a699858fd7832d13dc17a920aa820f2d39d4201
Gonzalo <gonzaa.vidal.18@gmail.com>
2022-07-07 07:02:09 -0300

    login

---
+++ assets/use-quasar.edf5e3c7.js
@@ -0,0 +1,1 @@
+import{q as a,ak as r}from"./index.833c13ce.js";function u(){return a(r)}export{u};
---
+++ assets/vuex.e1d62ca5.js
@@ -0,0 +1,1 @@
+import{b as a,s as o}from"./index.833c13ce.js";var r=a(async({app:s})=>{s.use(o)});export{r as default};
