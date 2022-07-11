4982a36f0285da40ff869db53111f3a9d329a82f
Gonzalo <gonzaa.vidal.18@gmail.com>
2022-07-11 15:07:22 -0300

    linkedin act

---
+++ assets/use-quasar.2ce3e012.js
@@ -0,0 +1,1 @@
+import{q as a,ah as r}from"./index.1570b396.js";function u(){return a(r)}export{u};
---
+++ assets/useRedes.f6c3ea07.js
@@ -0,0 +1,1 @@
+import{r as o}from"./database.b8731956.js";const a=e=>{o.forEach(r=>{if(e==r.red)return window.open(`${r.enlace}`,"_blank"),!0})};export{a as u};
