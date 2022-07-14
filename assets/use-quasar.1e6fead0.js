3ff67ab43f4b6601f2a7298984de231da74ad143
Gonzalo <gonzaa.vidal.18@gmail.com>
2022-07-14 02:42:13 -0300

    final panel

---
+++ assets/use-quasar.1e6fead0.js
@@ -0,0 +1,1 @@
+import{q as a,ah as r}from"./index.a41ae495.js";function u(){return a(r)}export{u};
---
+++ assets/vuex.781c6f79.js
@@ -0,0 +1,1 @@
+import{b as a,s as o}from"./index.a41ae495.js";var r=a(async({app:s})=>{s.use(o)});export{r as default};
