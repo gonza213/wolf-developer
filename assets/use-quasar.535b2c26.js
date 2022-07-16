387722a2900c43f1c5ba8cb9009a69c7488ea71c
Gonzalo <gonzaa.vidal.18@gmail.com>
2022-07-16 20:59:36 -0300

    validation

---
+++ assets/use-quasar.535b2c26.js
@@ -0,0 +1,1 @@
+import{p as a,ag as r}from"./index.f70018ef.js";function u(){return a(r)}export{u};
---
+++ assets/vuex.4ffeb7f3.js
@@ -0,0 +1,1 @@
+import{g as a,s as o}from"./index.f70018ef.js";var r=a(async({app:s})=>{s.use(o)});export{r as default};
