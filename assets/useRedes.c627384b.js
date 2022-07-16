1c8cf1e9ab061a4f336701cd8e070e0416c3c850
Gonzalo <gonzaa.vidal.18@gmail.com>
2022-07-15 23:00:32 -0300

    setup

---
+++ assets/useRedes.c627384b.js
@@ -0,0 +1,1 @@
+import{r as o}from"./database.4265c054.js";const a=e=>{o.forEach(r=>{if(e==r.red)return window.open(`${r.enlace}`,"_blank"),!0})};export{a as u};
---
+++ assets/vuex.d41f44e7.js
@@ -0,0 +1,1 @@
+import{g as a,s as o}from"./index.f4765a02.js";var r=a(async({app:s})=>{s.use(o)});export{r as default};
