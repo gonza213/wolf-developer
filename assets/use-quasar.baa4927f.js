d45b70054eae196b0615f3493485ab978014aab1
Gonzalo <gonzaa.vidal.18@gmail.com>
2022-07-19 03:40:46 -0300

    about

---
+++ assets/use-quasar.baa4927f.js
@@ -0,0 +1,1 @@
+import{p as a,ag as r}from"./index.267c8a97.js";function u(){return a(r)}export{u};
---
+++ assets/vuex.710c35a3.js
@@ -0,0 +1,1 @@
+import{g as a,s as o}from"./index.267c8a97.js";var r=a(async({app:s})=>{s.use(o)});export{r as default};
