e76d69d13a6d927f2c81df88c894406e314a7013
Gonzalo <gonzaa.vidal.18@gmail.com>
2022-07-25 14:59:13 -0300

    new cv

---
+++ assets/use-quasar.13c2a2f4.js
@@ -0,0 +1,1 @@
+import{p as a,ag as r}from"./index.b17a3351.js";function u(){return a(r)}export{u};
---
+++ assets/vuex.4b826f46.js
@@ -0,0 +1,1 @@
+import{g as a,s as o}from"./index.b17a3351.js";var r=a(async({app:s})=>{s.use(o)});export{r as default};
