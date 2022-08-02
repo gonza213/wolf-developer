190e8da7d1d748187b0af82ddfad114ccb002ccb
Gonzalo <gonzaa.vidal.18@gmail.com>
2022-08-02 19:30:09 -0300

    act cv

---
+++ assets/use-quasar.e29676f0.js
@@ -0,0 +1,1 @@
+import{p as a,ag as r}from"./index.a4028430.js";function u(){return a(r)}export{u};
---
+++ assets/vuex.4502e67b.js
@@ -0,0 +1,1 @@
+import{g as a,s as o}from"./index.a4028430.js";var r=a(async({app:s})=>{s.use(o)});export{r as default};
